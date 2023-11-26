a358f47d921f205cb1c7c542d0a9bca28bb14445
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2023-11-26 18:29:32 +0000

    red: prints an account statement including the transactions made throughout the console

--- src/tests/acceptance/PrintStatement.test.ts
+++ src/tests/acceptance/PrintStatement.test.ts
@@ -1,22 +1,24 @@
 import { Account } from "../../core/Account";
 import {Console} from "../../core/Console"
+import { StatementPrinter } from "../../core/StatementPrinter";
 import { TransactionRepository } from "../../core/TransactionRepository";
 
 describe('Print statement', ()=>{
     const console = new Console();
     const consoleSpy = jest.spyOn(console, 'log');
     const repository = new TransactionRepository();
-    const account: Account = new Account(repository);
-  
-  it('prints an account statement including the transactions made throughout the console', () => {
-    account.deposit(1000);
-    account.withdraw(500);
-    account.deposit(2000);
-    account.printStatement();
+    const statementPrinter = new StatementPrinter();
+    const account = new Account(repository, statementPrinter);
     
-    expect(consoleSpy).toHaveBeenCalledWith('Date | Amount | Balance');
-    expect(consoleSpy).toHaveBeenCalledWith('14/01/2022 | 2000 | 2500');
-    expect(consoleSpy).toHaveBeenCalledWith('13/01/2022 | -500 | 500');
-    expect(consoleSpy).toHaveBeenCalledWith('10/01/2022 | 1000 | 1000');
-  });
+    it('prints an account statement including the transactions made throughout the console', () => {
+        account.deposit(1000);
+        account.withdraw(500);
+        account.deposit(2000);
+        account.printStatement();
+        
+        expect(consoleSpy).toHaveBeenCalledWith('Date | Amount | Balance');
+        expect(consoleSpy).toHaveBeenCalledWith('14/01/2022 | 2000 | 2500');
+        expect(consoleSpy).toHaveBeenCalledWith('13/01/2022 | -500 | 500');
+        expect(consoleSpy).toHaveBeenCalledWith('10/01/2022 | 1000 | 1000');
+    });
 })
